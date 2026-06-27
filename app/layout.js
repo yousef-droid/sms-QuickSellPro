3831e1d3a8789cf4ab639f5439006f01d0c7e31b
yousef-droid <yousefashry11@gmail.com>
2026-06-27 21:49:58 +0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'QuickSellPro - Number Activation',
+  description: 'Digital Number Activation Platform',
+}
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" dir="ltr">
+      <body>{children}</body>
+    </html>
+  )
+}
